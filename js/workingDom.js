a97047eee1fdf9ee9c9ce77b42b53b3627f5efd7
Asilbek2706 <249135167+Asilbek2706@users.noreply.github.com>
2026-02-18 01:35:29 +0500

    Working with DOM

---
+++ js/workingDom.js
@@ -0,0 +1,46 @@
+const box = document.querySelector('#box'),
+    buttons = document.querySelectorAll('button'),
+    // circles = document.querySelectorAll('.circle'),
+    circles = document.getElementsByClassName('circle'),
+    wrapper = document.querySelector('.wrapper'),
+    hearts = wrapper.querySelectorAll('.heart')
+
+// box.style.backgroundColor = 'red';
+// box.style.width = '400px';
+//
+// buttons[1].style.width = '200px';
+//
+// circles[0].style.borderRadius = '0';
+// circles[1].style.cssText = 'background-color: red';
+
+// for (let i = 0; i < circles.length; i++) {
+//     circles[i].style.borderRadius = '0'
+// }
+//
+// buttons.forEach((item, index) => {
+//     item.style.borderRadius = '100%'
+// })
+
+
+// const div = document.createElement('div')
+// div.style.backgroundColor = 'black'
+// div.classList.add('heart')
+// document.body.append(div)
+// wrapper.append(div)
+// wrapper.prepend(div)
+// console.log(div)
+
+// circles[0].remove()
+// hearts[0].replaceWith(circles[1])
+
+// const div = document.createElement('div')
+// div.style.width = '400px'
+// div.style.height = '200px'
+// div.style.backgroundColor = 'red'
+// document.querySelector('.app').append(div)
+// div.innerHTML = '<h1>Hello World!</h1>'
+// div.textContent = 'Hello World!'
+// div.insertAdjacentHTML("afterend", "<h1>Hello World</h1>")
+// div.insertAdjacentHTML("afterbegin", "<h1>Hello World</h1>")
+// div.insertAdjacentHTML("beforeend", "<h1>Hello World</h1>")
+// div.insertAdjacentHTML("beforebegin", "<h1>Hello World</h1>")
