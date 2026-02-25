901be49fa9f78781623a8da6f02a8a24033dae0c
Asilbek2706 <249135167+Asilbek2706@users.noreply.github.com>
2026-02-25 07:19:51 +0500

    Document and Window parameters

---
+++ document-window/script.js
@@ -0,0 +1,24 @@
+
+
+/*
+const box = document.querySelector('.box'),
+    btn = document.querySelector('button')
+
+console.log(window.getComputedStyle(box))
+
+console.log(box.getBoundingClientRect())
+
+btn.addEventListener('click', (e) => {
+
+    box.style.height = box.scrollHeight + 'px'
+    console.log(box.scrollTop)
+})
+
+const width = box.clientWidth
+const height = box.clientHeight
+
+const width = box.offsetWidth;
+const height = box.offsetHeight;
+
+const width = box.scrollWidth;
+const height = box.scrollHeight;*/
